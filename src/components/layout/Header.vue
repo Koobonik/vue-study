1ab16c9cebd3f31afb6c0169c13debce0b418edc
Koobonik <kbi960130@naver.com>
2021-02-02 00:45:13 +0900

    링크 추가

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-brand href="#">People Who Make the better World</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <b-nav-item href="#">소개</b-nav-item>
+          <b-nav-item href="/member" >멤버소개</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
